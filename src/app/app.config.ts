fccd26e6e6dfc5ec5ae88df6f91c2da3f7950172
Nada Hassan <nada.h22425@gmail.com>
2025-06-27 23:08:50 +0300

    install prime ng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,26 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      ripple: true,
+      theme: {
+        preset: Aura
+      },
+      zIndex: {
+        modal: 1100,    // dialog, sidebar
+        overlay: 1000,  // dropdown, overlaypanel
+        menu: 1000,     // overlay menus
+        tooltip: 1100   // tooltip
+      }
+    })
+  ]
 };
