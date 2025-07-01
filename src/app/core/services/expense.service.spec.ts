7b54343993f7b4fe838df1c2cb5ba86b58149333
Nada Hassan <nada.h22425@gmail.com>
2025-07-01 03:06:43 +0300

    unit testing for add expense and get all expenses

--- src/app/core/services/expense.service.spec.ts
+++ src/app/core/services/expense.service.spec.ts
@@ -1,16 +1,106 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ExpenseService } from './expense.service';
+import { ExpenseI } from '@interfaces/expense';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
-describe('ExpenseService', () => {
-  let service: ExpenseService;
+const expense: ExpenseI = {
+  category: {
+    id: 'string',
+    name: 'string',
+    createdAt: 'string',
+    icon: 'string',
+  },
+  date: '12/12/2025',
+  amount: 1000,
+  currencyRate: 24,
+
+  receipt: 'file.png',
+  usdAmount: 20,
+
+};
+fdescribe('ExpenseService', () => {
+  let expenseService: ExpenseService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ExpenseService);
+
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(withInterceptorsFromDi()),
+        provideHttpClientTesting(),
+        ExpenseService
+      ]
+    });
+
+    expenseService = TestBed.inject(ExpenseService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('expense service should be created', () => {
+    expect(expenseService).toBeTruthy();
+  });
+
+  describe('API URL', () => {
+    it('should have correct API URL', () => {
+      expect(expenseService.API_URL).toEqual('https://6861cc6296f0cc4e34b77cae.mockapi.io/api/v1');
+    });
+
+  });
+
+  it('should fetch all expenses', () => {
+    const mockResponse: ExpenseI[] = [expense];
+
+
+    expenseService.getAllExpenses().subscribe(response => {
+
+      expect(response.length).toBeGreaterThan(0);
+      expectExpenseType(response[0]);
+
+    });
+
+    const req = httpMock.expectOne(`${expenseService.API_URL}/expenses`);
+    expect(req.request.method).toBe('GET');
+    expect(req.request.body).toBeNull();
+    req.flush(mockResponse);
+  });
+
+  it('should create an expense', () => {
+    const mockResponse: ExpenseI = expense;
+
+    expenseService.addExpense(expense).subscribe(response => {
+
+      expectExpenseType(response);
+
+    });
+
+    const req = httpMock.expectOne(`${expenseService.API_URL}/expenses`);
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(expense);
+    req.flush(mockResponse);
   });
 });
+
+function expectExpenseType(actual: ExpenseI) {
+  expect(actual).toEqual(jasmine.objectContaining({
+
+    category: {
+      id: jasmine.any(String),
+      name: jasmine.any(String),
+      createdAt: jasmine.any(String),
+      icon: jasmine.any(String),
+    },
+    date: jasmine.any(String),
+    amount: jasmine.any(Number),
+    currencyRate: jasmine.any(Number),
+
+    receipt: jasmine.any(String),
+    usdAmount: jasmine.any(Number),
+  }));
+}
+
